5b5b205b851c883281c8fb18873f1ba72d0bf229
Gabriel Santos Souto <107152342+gabrielsantossouto@users.noreply.github.com>
2025-03-21 09:36:49 -0300

    a

--- aula15ex/script.js
+++ aula15ex/script.js
@@ -1,6 +1,6 @@
 let num = document.querySelector('input#num')
 let lista = document.querySelector('select#flist')
-let res = document.querySelector('div#res')
+let res = document.getElementById('resultado')
 let valores = []
 
 function isNumero (n) {
@@ -21,8 +21,26 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     }else {
         window.alert('[ERRO] Valor invalido ou ja encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if(valores.length == 0){
+        window.alert('[ERRO] Falta de dados')
+    } else {
+        let tot = valores.length
+
+
+        res.innerHTML = ''
+        res.innerHTML += `temos ${tot} valores`
     }
 }
